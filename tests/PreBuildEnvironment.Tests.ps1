8c998ccba2d0aad07c7488b5399b1aa145a84ffe
Stuart Moore <stuart@stuart-moore.com>
2017-07-23 16:26:35 +0100

    1

--- tests/PreBuildEnvironment.Tests.ps1
+++ tests/PreBuildEnvironment.Tests.ps1
@@ -1,8 +1,8 @@
 
 Describe "Pre Build Environment Tests" {
     Context "Test we have a valid config and Environment" {
-        It "Should work" {
-            1 | Should Be 1
+        It "EnvironmentConfig.json should exist" {
+            'c:\github\lab-build\config\EnvironmentConfig.json' | Should Exist
         }
     }
 }
