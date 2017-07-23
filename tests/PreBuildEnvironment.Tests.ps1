8166c578fdb2cf138569d6a9a1fd69c5808525aa
Stuart Moore <stuart@stuart-moore.com>
2017-07-23 16:04:56 +0100

    M

--- tests/PreBuildEnvironment.Tests.ps1
+++ tests/PreBuildEnvironment.Tests.ps1
@@ -1,34 +1,36 @@
 
-Context "Test we have a valid config and Environment" {
-    It "EnvironmentConfig.json should exist" {
-        "c:\github\lab-build\config\EnvironmentConfig.json" | Should Exist
-    }
-    $config = ConvertFrom-JSON -InputObject (Get-Content -Path "c:\github\lab-build\config\EnvironmentConfig.json" -raw) -WarningVariable WarnVar -ErrorVariable ErrorVar    
-    It "Config file should be valid JSON and import cleanly" {
-        ($ErrorVar[0] -eq $null )| Should Be $True
-        ($WarnVar[0] -eq $null) | Should Be $True    
-    }
-    It "Config should contain at least 2 environments" {
-        $config.environments.count | Should BeGreaterThan 1
-    }
-    It "Config Should contain at least 5 databases" {
-        $config.databases.count | Should BeGreaterThan 4
-    }
-    ForEach ($Environment in $config.environments ){
-        $sqlsvr = New-Object -TypeName  Microsoft.SQLServer.Management.Smo.Server($Environment.InstanceName)
-
-        It "Environment $($Environment.EnvironmentName) should be connectable" {
-            ($sqlsvr.Status -ne $null) | Should Be $True
+Describe "Pre Build Environment Tests" {
+    Context "Test we have a valid config and Environment" {
+        It "EnvironmentConfig.json should exist" {
+            "c:\github\lab-build\config\EnvironmentConfig.json" | Should Exist
         }
-        if ($sqlsvr.Status -ne $null){
-            Foreach ($Database in ($config.databases | Where-Object {$_.Environment -eq $Environment.EnvironmentName })) {
-                It "$($Database.DatabaseName) Should Not exist on $($Environment.EnvironmentName)"{
-                    ($Database.DatabaseName -in $sqlsvr.Databases.name) | Should be $False
+        $config = ConvertFrom-JSON -InputObject (Get-Content -Path "c:\github\lab-build\config\EnvironmentConfig.json" -raw) -WarningVariable WarnVar -ErrorVariable ErrorVar    
+        It "Config file should be valid JSON and import cleanly" {
+            ($ErrorVar[0] -eq $null )| Should Be $True
+            ($WarnVar[0] -eq $null) | Should Be $True    
+        }
+        It "Config should contain at least 2 environments" {
+            $config.environments.count | Should BeGreaterThan 1
+        }
+        It "Config Should contain at least 5 databases" {
+            $config.databases.count | Should BeGreaterThan 4
+        }
+        ForEach ($Environment in $config.environments ){
+            $sqlsvr = New-Object -TypeName  Microsoft.SQLServer.Management.Smo.Server($Environment.InstanceName)
+
+            It "Environment $($Environment.EnvironmentName) should be connectable" {
+                ($sqlsvr.Status -ne $null) | Should Be $True
+            }
+            if ($sqlsvr.Status -ne $null){
+                Foreach ($Database in ($config.databases | Where-Object {$_.Environment -eq $Environment.EnvironmentName })) {
+                    It "$($Database.DatabaseName) Should Not exist on $($Environment.EnvironmentName)"{
+                        ($Database.DatabaseName -in $sqlsvr.Databases.name) | Should be $False
+                    }
                 }
             }
-        }
-        else {
-            Write-Host "Skipping Db checks as instance not available"
+            else {
+                Write-Host "Skipping Db checks as instance not available"
+            }
         }
     }
 }
