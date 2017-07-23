bdb987816e87c37f8dd370d309302cb9bdd724ac
Stuart Moore <stuart@stuart-moore.com>
2017-07-23 16:21:39 +0100

    elimination

--- tests/PreBuildEnvironment.Tests.ps1
+++ tests/PreBuildEnvironment.Tests.ps1
@@ -1,10 +1,10 @@
 
 Describe "Pre Build Environment Tests" {
     Context "Test we have a valid config and Environment" {
-        It "EnvironmentConfig.json should exist" {
+ <#       It "EnvironmentConfig.json should exist" {
             'c:\github\lab-build\config\EnvironmentConfig.json' | Should Exist
         }
-        $config = ConvertFrom-JSON -InputObject (Get-Content -Path "c:\github\lab-build\config\EnvironmentConfig.json" -raw) -WarningVariable WarnVar -ErrorVariable ErrorVar    
+#>        $config = ConvertFrom-JSON -InputObject (Get-Content -Path "c:\github\lab-build\config\EnvironmentConfig.json" -raw) -WarningVariable WarnVar -ErrorVariable ErrorVar    
         It "Config file should be valid JSON and import cleanly" {
             ($ErrorVar[0] -eq $null )| Should Be $True
             ($WarnVar[0] -eq $null) | Should Be $True    
