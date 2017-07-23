4a7cdec96e7a9bfbe3084ed5541f81f161d656c0
Stuart Moore <stuart@stuart-moore.com>
2017-07-23 16:02:20 +0100

    M

--- tests/PreBuildEnvironment.Tests.ps1
+++ tests/PreBuildEnvironment.Tests.ps1
@@ -1,28 +1,28 @@
 
-Describe "Test we have a valid config and Environment" {
-    Context "EnvironmentConfig.json should exist" {
+Context "Test we have a valid config and Environment" {
+    It "EnvironmentConfig.json should exist" {
         "c:\github\lab-build\config\EnvironmentConfig.json" | Should Exist
     }
     $config = ConvertFrom-JSON -InputObject (Get-Content -Path "c:\github\lab-build\config\EnvironmentConfig.json" -raw) -WarningVariable WarnVar -ErrorVariable ErrorVar    
-    Context "Config file should be valid JSON and import cleanly" {
+    It "Config file should be valid JSON and import cleanly" {
         ($ErrorVar[0] -eq $null )| Should Be $True
         ($WarnVar[0] -eq $null) | Should Be $True    
     }
-    Context "Config should contain at least 2 environments" {
+    It "Config should contain at least 2 environments" {
         $config.environments.count | Should BeGreaterThan 1
     }
-    Context "Config Should contain at least 5 databases" {
+    It "Config Should contain at least 5 databases" {
         $config.databases.count | Should BeGreaterThan 4
     }
     ForEach ($Environment in $config.environments ){
         $sqlsvr = New-Object -TypeName  Microsoft.SQLServer.Management.Smo.Server($Environment.InstanceName)
 
-        Context "Environment $($Environment.EnvironmentName) should be connectable" {
+        It "Environment $($Environment.EnvironmentName) should be connectable" {
             ($sqlsvr.Status -ne $null) | Should Be $True
         }
         if ($sqlsvr.Status -ne $null){
             Foreach ($Database in ($config.databases | Where-Object {$_.Environment -eq $Environment.EnvironmentName })) {
-                Context "$($Database.DatabaseName) Should Not exist on $($Environment.EnvironmentName)"{
+                It "$($Database.DatabaseName) Should Not exist on $($Environment.EnvironmentName)"{
                     ($Database.DatabaseName -in $sqlsvr.Databases.name) | Should be $False
                 }
             }
