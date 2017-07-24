231b5dd607234c2079f3a34a936323f8fe15d7a8
Stuart Moore <stuart@stuart-moore.com>
2017-07-24 07:50:03 +0100

    m

--- build.ps1
+++ build.ps1
@@ -1,5 +1,9 @@
-Sconfig = ConvertFrom-JSON -InputObject (Get-Content .\config\EnvironmentConfig.json -raw)
+Sconfig = ConvertFrom-JSON -InputObject (Get-Content c:\github\lab-build\config\EnvironmentConfig.json -raw)
 New-Item C:\github\app-lab\backups\RestoreTimeClean\ -ItemType Directory
 #restoretimeClean
 $instance = $config.EnvironmentConfig | Where-Object {$_.EnvironmentName -eq $config.databases}
+$start = Get-date
 Invoke-SqlCmd -ServerInstance localhost\sql2016 -InputFile "C:\github\lab-build\buildscripts\RestoreTimeClean\RestoreTimeClean.sql" -QueryTimeout 0
+$end  = get-date
+$span = ($end-$start).minutes
+write-host "took $span minutes"
